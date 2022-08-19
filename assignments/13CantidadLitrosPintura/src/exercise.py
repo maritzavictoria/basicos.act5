72b369c9962e99f3b187b8d285931c379097ba9c
maritzavictoria <111372423+maritzavictoria@users.noreply.github.com>
2022-08-19 10:04:08 -0600

    Update exercise.py

--- assignments/13CantidadLitrosPintura/src/exercise.py
+++ assignments/13CantidadLitrosPintura/src/exercise.py
@@ -1,7 +1,10 @@
 import math
 def main():
-    #escribe tu código abajo de esta línea
-
+    area=float(input('Area a pintar en metros:'))  
+    rend=float(input('Rendimiento (m2/1):'))    
+    litros= area/rend 
+    res=math.cell(litros)
+    print=(f'litros a comprar: {res}')
 
 if __name__ == '__main__':
     main()
